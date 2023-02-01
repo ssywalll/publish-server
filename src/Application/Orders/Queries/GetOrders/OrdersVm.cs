b8837e6562eca85e5a6fbd8a1fe919d2c9506db8
Abdvllahracata <abdullahracata104@gmail.com>
2023-02-01 10:44:16 +0700

    Buat Table Order

---
+++ src/Application/Orders/Queries/GetOrders/OrdersVm.cs
@@ -0,0 +1,8 @@
+namespace CleanArchitecture.Application.Orders.Queries.GetOrders
+{
+    public class OrdersVm
+    {
+        public string Status { get; set; } = string.Empty;
+         public IList<OrderDto> Data { get; set; } = new List<OrderDto>();
+    }
+}
