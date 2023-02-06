8eb176f2c3c110c43a42ce8c9d71e4455bcef51a
ssywalll <noorsyawal6@gmail.com>
2023-02-06 10:20:12 +0700

    GetHedaer

--- src/Application/BankAccounts/Queries/ExportBankAccounts.cs/GetBankAccountByToken.cs
+++ src/Application/BankAccounts/Queries/ExportBankAccounts.cs/GetBankAccountByToken.cs
@@ -4,7 +4,6 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using CleanArchitecture.Application.BankAccounts.Queries.GetBankAccounts;
