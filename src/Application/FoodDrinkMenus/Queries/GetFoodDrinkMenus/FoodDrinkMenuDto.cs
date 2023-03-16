424e427b888e6cae8f2cb062314391839c2d7a62
Abdvllahracata <abdullahracata104@gmail.com>
2023-03-16 09:33:51 +0700

    Add FoodDrink_Type on Dto

--- src/Application/FoodDrinkMenus/Queries/GetFoodDrinkMenus/FoodDrinkMenuDto.cs
+++ src/Application/FoodDrinkMenus/Queries/GetFoodDrinkMenus/FoodDrinkMenuDto.cs
@@ -4,16 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using CleanArchitecture.Application.Common.Mappings;
 using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Enums;
 
 namespace CleanArchitecture.Application.FoodDrinkMenus.Queries.GetFoodDrinkMenus
 {
     public class FoodDrinkMenuDto : IMapFrom<FoodDrinkMenu>
     {
-        public int Id { get ; set; }
+        public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public float Price { get; set; }
         public int Min_Order { get; set; }
         public string Description { get; set; } = string.Empty;
         public string Image_Url { get; set; } = string.Empty;
+        public FoodDrink_type FoodDrink_Type { get; set; }
+
     }
 }
